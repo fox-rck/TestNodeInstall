a8184109afb0b4ae9310a02498a10a6fb910c81a
fox-rck <fox.rck@gmail.com>
2015-06-05 18:07:41 -0500

    Update app.js

--- app.js
+++ app.js
@@ -2,5 +2,5 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello Node.js\n');
-}).listen(8124, "127.0.0.1");
+}).listen(6001, "127.0.0.1");
 console.log('Server running at http://127.0.0.1:6001/');
